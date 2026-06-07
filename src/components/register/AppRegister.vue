3172f7f63f6c21327d9fca6a109b8b708c915c58
hjxxzk <273017@student.pwr.edu.pl>
2026-06-07 14:44:28 +0200

    Add routing

---
+++ src/components/register/AppRegister.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>App register</h1>
+</template>
+
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AppHome from '@/components/home/AppHome.vue'
+import AppLogin from '@/components/login/AppLogin.vue'
+import AppRegister from '@/components/register/AppRegister.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: AppHome,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: AppLogin,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: AppRegister,
+    },
+  ],
 })
 
 export default router
